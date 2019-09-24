c4fb959886592c08ca798de059f3df5d3f00bcc5
jwucong <jwucong@gmail.com>
2019-09-24 18:08:15 +0800

    init

---
+++ src/components/H5/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="h5">
+		<component
+			v-for="module in modules"
+			:is="module.component"
+			:key="module.id"
+			:data="module.data"
+		>
+		</component>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'H5',
+	props: {
+		data: {
+			types: Object,
+			default: () => ({})
+		}
+	},
+	computed: {
+		modules() {
+			return this.data.components || []
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import H5 from '@/components/H5'
+
+export default {
+	name: 'home',
+	components: {
+		H5
+	}
+}
+</script>
